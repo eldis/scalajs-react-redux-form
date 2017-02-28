38833d6258a429132034195ac9b36683b9c2a34c
Dmitry Polienko <dmitry@eldis.ru>
2017-02-28 17:23:39 +0700

    Add chainReducers - a custom combineReducers alternative.

---
+++ src/main/scala/util/package.scala
@@ -0,0 +1,7 @@
+package eldis.redux.rrf
+
+/**
+ * Various useful functions that don't use RRF API as such,
+ * but are implemented using functionality provided by this package.
+ */
+package object util
